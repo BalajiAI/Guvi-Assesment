dfaff384d75f6549caba4b51cd129ff4e8f2ca3e
unknown <balajivd01@gmail.com>
2022-11-13 21:35:03 +0530

    Update

--- js/register.js
+++ js/register.js
@@ -1,11 +1,9 @@
-<html>
-
-<head> 
-<title> Index Page </title> 
-</head>
-
-<body>
- 
-</body>
-
-</html>
+function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=repassword]');
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+    } else {
+      confirm.setCustomValidity('Passwords do not match');
+    }
+  }
--- php/register.php
+++ php/register.php
@@ -1,29 +1,45 @@
+
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+
+</script>
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "alfrednobel";
-$dbname = "profile_data";
+$password = "";
+$dbname = "user_data";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-$stmt = $conn->prepare("INSERT INTO profile_data_table (firstname, lastname, email, password) VALUES (:firstname, :lastname, :email, :password)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':password', $password);
-
-
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
+$name = $_POST['name'];
 $email = $_POST['email'];
-$password = $_POST['password'];
-$stmt->execute();
+$pass = $_POST['password'];
+$repass = $_POST['repassword'];
 
 
-echo "New records created successfully";
-$conn = null;
 
+$sql = "INSERT INTO tb_user_data (name, email, pass, repass)
+VALUES ('$name','$email','$pass','$repass')";
+
+
+
+if (mysqli_query($conn, $sql)) {
+    echo '<script> alert("Successfully created LOGIN"); window.location.href="http://localhost/GUVI%20assisment/login.html";</script>';
+} else {
+    $er =  "Duplicate entry '". $email .  "' for key 'PRIMARY'";     
+    if($er== mysqli_error($conn))
+    {        
+        echo '<script> alert("User already exists please login"); window.location.href="http://localhost/GUVI%20assisment/login.html";</script>';        
+    }
+  else
+  {
+    echo mysqli_error($conn);
+  }
+}
+
+mysqli_close($conn);
 ?>
